a6eadefd911290b2dcb1196fe8356b6444f33a2b
Deniz Alp Keysan <deniz.keysan@trt.net.tr>
2022-08-25 00:16:12 +0300

    v1.0.1

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,12 @@
+RegisterCommand('gps', function(playerId, args, rawCommand)
+    local title = args[1]
+    local playerServerid = GetPlayerServerId(playerId)
+    -- TODO: get vehicle flash light state
+end)
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        
+
     end
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,5 +3,21 @@ game 'gta5'
 
 author 'Himeni'
 version '1.0.0'
+lua54 'yes'
 
-client_script 'client.lua'
+dependencies {
+    'es_extended',
+    'esx_menu_default'
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'config.lua',
+    'server/main.lua'
+}
